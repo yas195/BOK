a29b8a460bec66f34691adc1a12fbee2c429f652
yas195 <35342129+yas195@users.noreply.github.com>
2018-07-25 20:57:21 +0900

    Update 16-04-setup.sh

--- 16-04-setup.sh
+++ 16-04-setup.sh
@@ -32,8 +32,6 @@ echo '*** 진행중 2/3 ***'
 echo '*** 복코인드 시작 및 종료, 설정 ***'
 sleep 2
 ./bokcoind -daemon
-sleep 2
-./bokcoind -stop
 sleep 3
 echo -n "사용하고자 하는 rpc 패스워드를 입력해 주세요 입력후 [ENTER]: "
 read usrpas
@@ -43,9 +41,10 @@ echo -n "이 컴퓨터의 IP:46996 입력 (예: 123.456.789.111:46996) 입력후
 read ipaddress
 
 echo -e "rpcuser=bokuser \nrpcpassword=$usrpas \nrpcallowip=127.0.0.1 \nlisten=1 \nserver=1 \ndaemon=1 \nstaking=0 \nmasternodeaddr=$ipaddress \nmasternode=1 \nmasternodeprivkey=$mngenkey \n" > ~/.bokcoin/bokcoin.conf
-
-echo '*** 완료 2/3 ***'
 sleep 2
+./bokcoind -stop
+echo '*** 완료 2/3 ***'
+sleep 3
 echo '*** 단계 3/3 ***'
 echo '*** 복코인드 시작 및 지갑 싱크 시작 ***'
 echo '1분후 getinfo 정보가 출력될 것입니다...'
